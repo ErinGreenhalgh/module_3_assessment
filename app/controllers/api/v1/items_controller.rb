13281c99189f06be3f57087df04b072a7e6bcd84
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-08-03 10:17:07 -0600

    moving on from create method * setup seems correct but getting this error: ActionController::ParameterMissing Exception: param is missing or the value is empty: item * have set protect_from_forgery with: to :null_session * can't figure out what's going on

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ItemsController < ApplicationController
+  respond_to :json
   def index
     render json: Item.all
   end
@@ -13,12 +14,12 @@ class Api::V1::ItemsController < ApplicationController
 
   def create
     byebug
-    render json: Item.create(params["item"]), status: 201
+    render json: Item.create(item_params), status: 201
   end
 
   private
-  # def item_params
-  #   params.require(:item).permit(:name, :description, :image_url)
-  # end
+  def item_params
+    params.require(:item).permit(:name, :description, :image_url)
+  end
 
 end
