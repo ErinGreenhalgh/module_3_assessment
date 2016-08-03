817f01ae45cee0279316067848ecf797998e5b21
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2016-08-03 10:55:52 -0600

    add close stores method in best buy service and add test * still having trouble with making api call through postman, can make example call work, but not sure what is different when I make a call with different zip and params * carrying on anyway

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,4 +1,21 @@
 class BestBuyService
-  
+
+  def initialize
+    @_connection = Faraday.new("https://api.bestbuy.com")
+  end
+
+  def close_stores(zipcode)
+    _connecion.params = {"format" => "json",
+                         "apiKey" => ENV["api_key"],
+                         "show" => "longName,city,distance,phone,storeType"}
+    response = _connection.get("/v1/stores(area(#{zipcode},25))")
+    JSON.parse(response.body)
+  end
+
+
+  private
+  def _connection
+    @_connection
+  end
 
 end
--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -3,7 +3,10 @@ require "rails_helper"
 RSpec.describe "best buy service" do
   scenario "gets close stores to a given zipcode"
     VCR.use_cassette("close_stores") do
-      
+      key_data = "longName", "city", "distance", "phone", "storeType"
+
+      expect(data["stores"].count).to eq 15
+      expect(data["stores"].first.keys).to eq
     end
   end
 end
